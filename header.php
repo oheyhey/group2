b71414f98c192e84410b4d6d23b4022ad1f0fb01
oheyhey <50399770+oheyhey@users.noreply.github.com>
2019-05-14 03:16:59 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- saved from url=(0038)https://www.teamdurham.com/facilities/ -->
+<html class="no-js" lang="en"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Our Facilities - Durham University</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="shortcut icon" href="https://www.teamdurham.com/images/template/teamdurham/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="./try_files/team-durham.css" type="text/css">
+	<link rel="stylesheet" href="nav.css" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+
+		
+		
